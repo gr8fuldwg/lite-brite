3b8cf8ceed7d63a4c4a2013601b9683ebee35b72
JON J <gr8fuldwg@gmail.com>
2018-10-30 18:28:49 -0400

    created app.js sections

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+// variables
 
 
-function makeGrid(){
-    return [
-    Cell(`<div class="cell h30 w30 bl-none bt-none"></div>`), 
-    Cell(`<div class="cell h30 w30 bt-none"></div>`),
-    Cell(`<div class="cell h30 w30 bt-none br-none"></div>`),
-    Cell(`<div class="cell h30 w30 bl-none"></div>`),
-    Cell(`<div class="cell h30 w30"></div>`),
-    Cell(`<div class="cell h30 w30 br-none"></div>`),
-    Cell(`<div class="cell h30 w30 bl-none bb-none"></div>`),
-    Cell(`<div class="cell h30 w30 bb-none"></div>`),
-    Cell(`<div class="cell h30 w30 bb-none br-none"></div>`),
-    ];
-}
+// events
+
+
+// functions
+
+
+// kick it off
