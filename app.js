24281530a032c234ef408461ced6dc81cda8abc7
JON J <gr8fuldwg@gmail.com>
2018-10-30 20:38:03 -0400

    created clickable cells to change their colors to black in app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // variables
-
+const cells = document.querySelectorAll(".cell");
 
 // events
-
+cells.forEach(function attachOnClick(cell){
+  cell.addEventListener("click",changeColor);
+});
 
 // functions
-
+function changeColor(event){
+    const element = event.target;
+    element.style.backgroundColor = "black";
+}
 
 // kick it off
